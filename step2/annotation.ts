48741ec8ce3d0ddf32ce6fce10bf7fba1af845d7
Ilnar Gilyaev <gilyaev.if@gmail.com>
2022-01-03 16:53:08 +0500

    added enum for step2

--- step2/annotation.ts
+++ step2/annotation.ts
@@ -43,11 +43,69 @@ let greeting:(name:string)=>string;
 greeting = function(name:string) {
     return `Hi ${name}`;
 }
-
 // let greeting1:(name:string)=>string;
 // let message='Hello, World!';
 // greeting1 =function (name:string) {
 //     console.log(message);
 // }
 
+//Turple
+let bgColor, headerColor:[number,number,number,number?];
+bgColor=[255,20,0, 0.5];
+headerColor=[255,0,0];
+console.log(`bgColor=`,bgColor,'/',`headerColor=`,headerColor);
 
+//enum
+enum Month {
+    Jan,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec
+};
+console.log(Month);
+
+function isItSummer(month:Month){
+    let isSummer:boolean;
+    switch (month) {
+        case Month.Jun:
+        case Month.Jul:
+        case Month.Aug:
+            isSummer=true;
+            break;
+        default:
+            isSummer=false;
+            break;
+    }
+    return isSummer;
+}
+console.log(isItSummer(Month.Sep),isItSummer(Month.Jun));
+
+enum TimeOfDay {
+    x,
+    y,
+    b,
+    z
+}
+function isItNight(time:TimeOfDay){
+    let IsNight:boolean;
+    switch (time){
+        case TimeOfDay.x:
+        case TimeOfDay.y:
+        case TimeOfDay.b:
+            IsNight =true;
+            break;
+        default:
+            IsNight=false;
+            break;
+    }
+return IsNight;
+}
+console.log(isItNight(TimeOfDay.z));
