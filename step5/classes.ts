81328d18256ee9a656e80cfb16335d6f0a0536cf
Ilnar Gilyaev <gilyaev.if@gmail.com>
2022-01-04 15:09:34 +0500

    added some examples for classes 2

--- step5/classes.ts
+++ step5/classes.ts
@@ -35,6 +35,9 @@ class DogName {
 let dogName = new DogName('Marti', 'Gil');
 console.log(dogName.getFullDogName());
 
+let a = 1;
+let b = 2;
+
 class Sum {
     a: number;
     b: number;
@@ -49,7 +52,45 @@ class Sum {
     }
 }
 
-let sum = new Sum(1, 2);
-console.log(sum.getSum());
+let sum = new Sum(a, b);
+console.log('sum =', sum.getSum());
+console.log('a =', sum.a);
 
 //Access Modifiers
+// private
+// protected
+// public
+
+class Multiplication {
+    private a: number;
+    private b: number;
+
+    constructor(a: number, b: number) {
+        this.a = a;
+        this.b = b;
+    }
+
+    getMultiplication() {
+        return this.a * this.b;
+    }
+}
+
+let multiplication = new Multiplication(a, b);
+console.log('multiplication-private =', multiplication.getMultiplication()); //a,b are private and u can't access them without class Multiplicatipon
+
+class Multiplication1 {
+    public a: number; //u don't need to write public
+    public b: number; //u don't need to write public
+
+    constructor(a: number, b: number) {
+        this.a = a;
+        this.b = b;
+    }
+
+    getMultiplication1() {
+        return this.a * this.b;
+    }
+}
+
+let multiplication1 = new Multiplication1(a, b);
+console.log('multiplication-public =', multiplication1.a); //a,b are public and u can access them without class Multiplicatipon
