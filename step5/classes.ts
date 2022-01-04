9392387e4d6005ca53f13de2f3a978feb967f56f
Ilnar Gilyaev <gilyaev.if@gmail.com>
2022-01-04 20:46:17 +0500

    added some examples for classes

--- step5/classes.ts
+++ step5/classes.ts
@@ -105,4 +105,187 @@ class PersonProtected {
 }
 
 let personProtected = new PersonProtected('123', 'Iln', 'Gil');
-console.log(personProtected.getFullName());
+console.log(personProtected.getFullName()); //protected modifier allows access within the same class and subclasses.
+
+//Readonly
+class PersonReadOnly {
+    constructor(readonly birthDate: Date, public ssn: string, public firstName: string, public lastName: string) {
+        this.birthDate = birthDate;
+        this.ssn = ssn;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+}
+
+let personReadOnly = new PersonReadOnly(new Date(1990, 11, 12), '12', 'Il', 'Gil');
+console.log(personReadOnly.birthDate);
+//personReadOnly.birthDate=new Date(1992,10,15); //Cannot assign to 'birthDate' because it is a read-only property.
+
+//Getters and Setters (get and set)
+class PersonGetAndSet {
+    constructor(private _ssn: string, private _firstName: string, private _lastName: string) {
+        this._ssn = _ssn;
+        this._firstName = _firstName;
+        this._lastName = _lastName;
+    }
+
+    public get ssn() {
+        return this._ssn;
+    }
+
+    public set ssn(theSsn: string) {
+        if (!theSsn) {
+            throw new Error('Invalid ssn');
+        }
+        this._ssn = theSsn;
+    }
+
+    public get firstName() {
+        return this._firstName;
+    }
+
+    public set firstName(theFirstName: string) {
+        if (!theFirstName) {
+            throw new Error('Invalid First Name');
+        }
+        this._firstName = theFirstName;
+    }
+
+    public get lastName() {
+        return this._lastName;
+    }
+
+    public set lastName(theLastName: string) {
+        if (!theLastName) {
+            throw new Error('Invalid Last Name');
+        }
+        this._lastName = theLastName;
+    }
+
+    public get fullName() {
+        return `${this.firstName.toUpperCase()} ${this.lastName.toUpperCase()}`;
+    }
+
+    public set fullName(name: string) {
+        let parts = name.split('/ ');
+        if (parts.length < 4) {
+            throw new Error('Invalid name format: first last');
+        }
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+    }
+}
+
+let personGetAndSet = new PersonGetAndSet('sdf', 'il', 'gi');
+console.log('personGetAndSet =', personGetAndSet.fullName);//get and set with private
+
+class Division {
+    constructor(private a: number, private b: number) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public get division() {
+        return this.a / this.b
+    }
+
+    public set division(num: number) {
+        this.a = num + 2;
+    }
+}
+
+let division = new Division(2, 2);
+division.division = 12;
+console.log(division.division);
+
+class Person2 {
+    // ... other code
+    private firstName: string;
+    private lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    public set fullName(name: string) {
+        let parts = name.split(' ');
+        if (parts.length != 2) {
+            throw new Error('Invalid name format: first last');
+        }
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+    }
+}
+
+let person2 = new Person2('asd', 'sad');
+person2.fullName = 'John engsa';
+
+console.log(person2.fullName);
+
+//Inheritance
+class PersonInh {
+    constructor(private firstName: string, private lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    getFullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    describe(): string {
+        return `This is ${this.firstName} ${this.lastName}.`;
+    }
+}
+
+class Employee extends PersonInh {
+    constructor(firstname: string, lastname: string) {
+        super(firstname, lastname);
+    }
+}
+
+let employee = new Employee('John', 'Eng');
+console.log(employee.getFullName());
+console.log(employee.describe());
+
+class EmployeeMe extends PersonInh {
+    constructor(firstname: string, lastname: string) {
+        super(firstname, lastname);
+    }
+
+    describe(): string {
+        return `I am ${this.getFullName()}.`;
+    }
+}
+
+let employeeMe = new EmployeeMe('John1', 'Eng1');
+console.log(employeeMe.describe());
+
+//Static Methods and Properties
+class Employee1 {
+    private static headcount: number = 0;
+
+    constructor(
+        private firstName: string,
+        private lastName: string,
+        private jobTitle: string) {
+
+        Employee1.headcount++;
+    }
+
+    public static getHeadcount() {
+        return Employee1.headcount;
+    }
+}
+let john = new Employee1('John', 'Doe', 'Front-end Developer');
+let jane = new Employee1('Jane', 'Doe', 'Back-end Developer');
+
+console.log(Employee1.getHeadcount());
+
+//Abstract Classes
+
